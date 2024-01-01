1720b4a8e566f6736a8180f544d9e42b06632b6d
1iqingfeng <2390698445@qq.com>
2024-01-01 20:23:55 +0800

    可以使用了

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -1,32 +1,74 @@
+import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+
 
 public class demo {
-    public static void main(String[] args) {
-        // Create your bot passing the token received from @BotFather
-        TelegramBot bot = new TelegramBot("YOUR_API_TOKEN");
+    //    初始化日志
+    public static final Logger LOGGER = LoggerFactory.getLogger(demo.class);
+    static final String proxyHost = "127.0.0.1";
+    static final String proxyPort = "59829";
 
-// Register for updates
+    //初始化外网链接
+    static {
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
+    }
+
+    public static void main(String[] args) {
+        // 从@BotFather接收到的令牌创建您的机器人
+        TelegramBot bot = new TelegramBot("6374366540:AAF8te7OmhMKcTqlC97fig9xp_1z3TldEq0");
+
+        // 注册更新
         bot.setUpdatesListener(updates -> {
-            // ... process updates
-            // return id of last processed update or confirm them all
+
+            // 处理更新
+            // 提取消息
+
+            Message messaged = updates.get(0).message();
+//            messaged
+            LOGGER.info("messaged:{}", messaged.forwardFromMessageId());
+
+            LOGGER.info("messagedText:{}", messaged.text());
+
+            // 从消息创建一个消息
+
+            SendMessage msg = new SendMessage(messaged.chat().id(), "Hello!");
+
+            bot.execute(msg, new Callback<SendMessage, SendResponse>() {
+                @Override
+                public void onResponse(SendMessage request, SendResponse response) {
+                    if (!response.isOk()) {
+                        LOGGER.error("response:{}", response);
+                    } else {
+                        LOGGER.info("message:{}", response.message().from().id());
+                    }
+                }
+
+                @Override
+                public void onFailure(SendMessage request, IOException e) {
+                    LOGGER.error("e:{}", e.getMessage());
+                }
+            });
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
-// Create Exception Handler
+            // 创建异常处理程序
         }, e -> {
             if (e.response() != null) {
-                // got bad response from telegram
+                // 从Telegram收到错误响应
                 e.response().errorCode();
                 e.response().description();
             } else {
-                // probably network error
-                e.printStackTrace();
+                // 可能是网络错误
+                LOGGER.error("e:{}", e.getMessage());
             }
         });
-
-// Send messages
-        long chatId = update.message().chat().id();
-        SendResponse response = bot.execute(new SendMessage(chatId, "Hello!"));
     }
 }
